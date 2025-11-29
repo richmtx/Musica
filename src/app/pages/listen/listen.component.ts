e9f89f5e3115a5c0e28346847f0c4c40ad47fe51
rickymtx <rich.mtx1205@gmail.com>
2025-11-28 21:40:05 -0600

    Descripción de los cambios

---
+++ src/app/pages/listen/listen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listen',
+  imports: [],
+  templateUrl: './listen.component.html',
+  styleUrl: './listen.component.css'
+})
+export class ListenComponent {
+
+}
---
+++ src/app/pages/studio/studio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studio',
+  imports: [],
+  templateUrl: './studio.component.html',
+  styleUrl: './studio.component.css'
+})
+export class StudioComponent {
+
+}
