ab1eafd390d2c91d70f607b659be0355a14bb5dc
richmtx <martinezhernandezricardo06@gmail.com>
2026-01-12 14:42:21 -0600

    Commit

--- src/app/pages/listen/listen.component.ts
+++ src/app/pages/listen/listen.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  PLATFORM_ID
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
@@ -7,6 +14,35 @@ import { NavbarComponent } from "../../components/navbar/navbar.component";
   templateUrl: './listen.component.html',
   styleUrl: './listen.component.css'
 })
-export class ListenComponent {
+export class ListenComponent implements AfterViewInit {
 
+  constructor(
+    private el: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngAfterViewInit(): void {
+
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    const elements = this.el.nativeElement.querySelectorAll(
+      '.reveal, .reveal-delay'
+    );
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.2
+      }
+    );
+
+    elements.forEach((el: Element) => observer.observe(el));
+  }
 }
